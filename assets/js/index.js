23c6b19659254e91e7f011c2d53a7f6768ad7226
Drake Stephens <drake.stephens56@gmail.com>
2020-06-16 21:32:39 -0600

    added firebase

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,40 @@
+ // firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+    // Handle Errors here.
+   // var errorCode = error.code;
+   // var errorMessage = error.message;
+    // ...
+  // });
+function login(){
+  
+    var pwd = document.getElementById("pwd");
+    var eml = document.getElementById("email") 
+    if (eml.val() === "") {
+        $("#login-error").removeClass("hide")
+    }
+    else if (pwd.val() === "") {
+        $("#login-error").removeClass("hide")
+    }
+}
+  
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      var displayName = user.displayName;
+      var email = user.email;
+      var emailVerified = user.emailVerified;
+      var photoURL = user.photoURL;
+      var isAnonymous = user.isAnonymous;
+      var uid = user.uid;
+	  var providerData = user.providerData;
+	  console.log(user)
+      // ...
+    } else {
+      // User is signed out.
+	  // ...
+	  console.log("testing")
+    }
+  });
+
 function searchForMovies() {
     const title = document.getElementById('movie-title').value;
 
